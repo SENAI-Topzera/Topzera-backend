02741c0a65506f7be2caa991c00a74520110bbdb
Pedro Zwang <pedroh.zwang@gmail.com>
2022-05-16 19:34:50 -0300

    adicionando prisma ORM

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import express from "express";
-import carRoutes from "./routes/car";
-import statusRoute from "./routes/status";
 import cors from "cors";
 import dotenv from "dotenv";
+import { router } from "./routes";
 
 dotenv.config();
 const app = express();
 
-
 app.use(express.json());
 app.use(cors());
-app.use(carRoutes);
-app.use(statusRoute);
+app.use(router);
 app.listen(process.env.PORT);
